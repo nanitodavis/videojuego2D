09d0afe05ada53a070b54319a4c8969d279a8cfe
Nanito Davis <nanitodavis@gmail.com>
2016-04-20 12:38:03 -0500

    inicio del seguimiento de la camara

---
+++ videojuego2D/Assets/Scripts/player and camera/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public Transform target;
+	public float ahead;
+	public Vector3 newPos;
+	// Use this for initialization
+	void Start () {
+		ahead = target.transform.position.x + 3f;
+		newPos = new Vector3 (ahead, transform.position.y, transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ahead = target.transform.position.x + 3f;
+		newPos = new Vector3 (ahead, transform.position.y, transform.position.z);
+		transform.position = newPos;
+		
+	}
+}
