96b55f5a3feb76dd01bdc5d009fcd17fd21fe0f0
Nanito Davis <nanitodavis@gmail.com>
2016-05-07 10:52:22 -0500

    main char model and player warp

---
+++ videojuego2D/Assets/Scripts/CambioEscena.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CambioEscena : MonoBehaviour {
+	public GameObject escenario;
+	public GameObject player;
+	public Transform camera;
+	private ArrayList enemies;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
--- videojuego2D/Assets/Scripts/player and camera/PlayerScript.cs
+++ videojuego2D/Assets/Scripts/player and camera/PlayerScript.cs
@@ -3,30 +3,51 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 	private Rigidbody rb;
-	private GameObject player;
-	public float velocity = 15f;
+	public float velocity = 12f;
 	public float jumpForce = 180f;
+	private int health = 100;
+	private bool walk;
+	private bool run;
+	private bool jump = true;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
-		player = gameObject;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if(Input.GetKey(KeyCode.D)){
+		if(Input.GetKey(KeyCode.D)&&rb.velocity.x<=9f){
 			rb.AddForce(new Vector3(1,0,0)*velocity);
+		}
+		else if(Input.GetKey(KeyCode.A)&&rb.velocity.x>=-9f){
+			rb.AddForce(new Vector3(-1,0,0)*velocity);
 		}
 		else if(Input.GetKey(KeyCode.A)){
 			rb.AddForce(new Vector3(-1,0,0)*velocity);
 		}
 		if(Input.GetKeyDown(KeyCode.Space)){
 			rb.AddForce(new Vector3(0,1,0)*jumpForce);
+			//jump=false;
+		}
+		if(health<=0){
+			GameObject.Destroy(gameObject);
 		}
 	}
 
 	void OnCollisionEnter(Collision collision){
-		//if()
+		if (collision.gameObject.tag == "Enemy") {
+			health-=10;
+		}
+	}
+
+	void OnTriggerEnter(Collider collision){
+		if (collision.gameObject.tag == "Warp") {
+			Vector3 newPos = new Vector3(0,0,0);
+			transform.position=newPos;
+			//transform.position.x=0f;
+			//transform.position.y=0f;
+			//transform.position.z=0f;
+		}
 	}
 }
