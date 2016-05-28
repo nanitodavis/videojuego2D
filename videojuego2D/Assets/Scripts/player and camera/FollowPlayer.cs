f27b10c26dc3bb2c4f9cdbfa4fdc831b6458929a
nanito davis <nanitodavis@gmail.com>
2016-05-27 23:32:22 -0500

    arreglos a la camara y al salto del pj principal

--- videojuego2D/Assets/Scripts/player and camera/FollowPlayer.cs
+++ videojuego2D/Assets/Scripts/player and camera/FollowPlayer.cs
@@ -6,22 +6,31 @@ public class FollowPlayer : MonoBehaviour {
 	public Transform target;
 	public Vector3 ahead;
 	public Vector3 newPos;
+	public float offset = 3f;
 	// Use this for initialization
 	void Start () {
 		ahead = new Vector3 (0f, 0f, 0f);
-		ahead.x = target.transform.position.x + 3f;
-		ahead.y = transform.position.y;
-		ahead.z = transform.position.z;
-		newPos = new Vector3 (ahead.x, transform.position.y, transform.position.z);
+		ahead.x = target.transform.position.x + offset;
+		ahead.y = target.transform.position.y;
+		ahead.z = target.transform.position.z;
+		//newPos = new Vector3 (ahead.x, target.transform.position.y, target.transform.position.z);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		ahead.x = target.transform.position.x + 3f;
+		if((transform.position.x-target.transform.position.x)<offset&&(transform.position.x-target.transform.position.x)>(offset-1f)){
+			ahead.x = transform.position.x;
+		}
+		else if (transform.position.x-offset>=(target.transform.position.x)){
+			ahead.x -= 0.05f;
+		}
+		else if(transform.position.x-offset<=(target.transform.position.x)){
+			ahead.x += 0.05f;
+		}
+		//ahead.x = target.transform.position.x + offset;
 		ahead.y = target.transform.position.y + 4f;
-		ahead.z = target.transform.position.z + 4f;
+		ahead.z = target.transform.position.z + -5f;
 		newPos = new Vector3 (ahead.x, ahead.y, ahead.z);
 		transform.position = newPos;
-		
 	}
 }
--- videojuego2D/Assets/Scripts/player and camera/PlayerScript.cs
+++ videojuego2D/Assets/Scripts/player and camera/PlayerScript.cs
@@ -3,12 +3,12 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 	private Rigidbody rb;
-	public float velocity = 12f;
-	public float jumpForce = 180f;
+	public float velocity = 13f;
+	public float jumpForce = 250;
 	private int health = 100;
 	private bool walk;
 	private bool run;
-	private bool jump = true;
+	public bool jump = false;
 	private Transform modelTransform;
 	private GameObject childModel;
 
@@ -20,16 +20,16 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if(Input.GetKey(KeyCode.D)&&rb.velocity.x<=9f){
+		if(Input.GetKey(KeyCode.D)&&rb.velocity.x<=5f){
 			rb.AddForce(new Vector3(1,0,0)*velocity);
 		}
-		else if(Input.GetKey(KeyCode.A)&&rb.velocity.x>=-9f){
+		else if(Input.GetKey(KeyCode.A)&&rb.velocity.x>=13f){
 			rb.AddForce(new Vector3(-1,0,0)*velocity);
 		}
 		else if(Input.GetKey(KeyCode.A)){
 			rb.AddForce(new Vector3(-1,0,0)*velocity);
 		}
-		if(Input.GetKeyDown(KeyCode.Space)){
+		if(Input.GetKeyDown(KeyCode.Space)&&jump){
 			rb.AddForce(new Vector3(0,1,0)*jumpForce);
 			//jump=false;
 		}
@@ -42,17 +42,21 @@ public class PlayerScript : MonoBehaviour {
 		if (collision.gameObject.tag == "Enemy") {
 			health-=10;
 		}
+		if (collision.gameObject.tag == "Floor") {
+			jump = true;
+		}
+	}
+
+	void OnCollisionExit(Collision collision){
+		if (collision.gameObject.tag == "Floor") {
+			jump = false;
+		}
 	}
 
 	void OnTriggerEnter(Collider collision){
 		if (collision.gameObject.tag == "Warp") {
 			Vector3 newPos = new Vector3(53.34f,9.48f,-38f);
 			transform.position=newPos;
-			//modelTransform.rotation=new Vector3(0f, 180f, 0f);
-			//childModel.transform.rotation.y=modelTransform.rotation.y;
-			//transform.position.x=0f;
-			//transform.position.y=0f;
-			//transform.position.z=0f;
 		}
 	}
 }
